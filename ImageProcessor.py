7ab94bcdf87d738c3d5d18c21551cc200d5aeb47
Ryan <qirun_chen@163.com>
2017-09-27 18:43:00 +0100

    Modify the font file, and change the position of text.

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -7,8 +7,7 @@ from PIL import ImageFont
 class ImageProcessor:
     def __init__(self, output_path):
         self.image_path = os.getcwd() + os.sep + "img_template.jpg"
-        self.font1_path = os.getcwd() + os.sep + "msyh.ttf"
-        self.font2_path = os.getcwd() + os.sep + "Microsoft-Yahei-UI-Bold.ttc"
+        self.font1_path = os.getcwd() + os.sep + "msyh.ttf"#"simsun.ttc"
         self.output_path = output_path
         print self.image_path
 
@@ -16,7 +15,7 @@ class ImageProcessor:
         if not os.path.exists(self.image_path):
             return "image does not exist."
 
-        if not os.path.exists(self.font1_path) or not os.path.exists(self.font2_path):
+        if not os.path.exists(self.font1_path) or not os.path.exists(self.font1_path):
             return "font does not exist."
 
         self.edit_each_image(name_id_dict)
@@ -26,15 +25,13 @@ class ImageProcessor:
     def edit_each_image(self, name_id_dict):
         for name, id in name_id_dict.iteritems():
             image = Image.open(self.image_path)
-            if image.mode not in ('L', 'RGB'):
-                image = image.convert('RGB')
-
+            # if image.mode not in ('L', 'RGB'):
+            #     image = image.convert('RGB')
+            width, height = image.size
             draw = ImageDraw.Draw(image)
-            font1 = ImageFont.truetype(self.font1_path, 30)
-            font2 = ImageFont.truetype(self.font2_path, 32)
-            draw.text((301, 98), name, (0, 0, 0), font=font1)
-            draw.text((300, 170), id, (0, 0, 0), font=font1)
-            draw.text((301, 238), u"认证成功", (0, 0, 0), font=font2)
+            font1 = ImageFont.truetype(self.font1_path, 17)
+            draw.text((155, 45), name, (0, 0, 0), font=font1)
+            draw.text((155, 77), id, (0, 0, 0), font=font1)
             draw = ImageDraw.Draw(image)
 
             save_path = '%s/%s' % (self.output_path, name + "_" + id + ".jpg")
@@ -42,4 +39,7 @@ class ImageProcessor:
             if not os.path.exists:
                 os.makedirs(self.output_path)
 
-            image.save(save_path)
+            image.resize(image.size, Image.ANTIALIAS)
+            image.save(save_path, 'jpeg', quality=100)
+
+
